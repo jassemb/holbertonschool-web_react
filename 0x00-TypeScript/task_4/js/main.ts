eed6077afecf976490455f9a1dc293366b504150
jassemb <benalijassem201@gmail.com>
2022-09-26 11:53:57 +0100

    type script

--- 0x00-TypeScript/task_4/js/main.ts
+++ 0x00-TypeScript/task_4/js/main.ts
@@ -1,4 +1,4 @@
-/// <reference path="Cpp" />
+/// <reference path="./subjects/Cpp.ts" />
 /// <reference path="./subjects/Java.ts" />
 /// <reference path="./subjects/React.ts" />
 /// <reference path="./subjects/Subject.ts" />
