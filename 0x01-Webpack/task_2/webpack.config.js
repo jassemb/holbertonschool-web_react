05f05bf58e218b475ae46012a232af266f263cf3
jassemb <benalijassem201@gmail.com>
2022-10-07 14:50:24 +0100

    task 2 style css

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+      type: "asset",
+    },
+  ],},
+};   
