c1d04fdf0a87d4a6c8b07a7b86bde3c88dc4174a
jassemb <benalijassem201@gmail.com>
2022-10-22 00:09:51 +0100

    check done

--- 0x02-react_intro/task_5/dashboard/src/index.js
+++ 0x02-react_intro/task_5/dashboard/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App/App.js';
+import {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App/App.js";
+import Notifications from "./Notifications/Notifications.js"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
+  document.getElementById("root")
 );
