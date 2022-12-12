71b440850375634fc327a26960927477db24b0d2
Mock Student <florian.bolgar@wimbeetech.com>
2022-12-12 09:55:59 +0100

    map

--- 0x07-React_Immutable/1-map.js
+++ 0x07-React_Immutable/1-map.js
@@ -4,9 +4,9 @@ const getImmutableObject = (object) => Map(object);
 
 export default getImmutableObject;
 const obj = {
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
+	fear: true,
+	smell: -1033575916.9145899,
+	wall: false,
+	thing: -914767132
 }
 console.log(getImmutableObject(obj));
