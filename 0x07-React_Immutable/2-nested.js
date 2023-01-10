f22fc765bcfbbc792fac959d8643341df8a71828
jassemb <benalijassem201@gmail.com>
2023-01-10 16:32:01 +0100

    test

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -3,4 +3,4 @@ import { fromJS, getIn } from 'immutable';
 export default function accessImmutableObject(object, array) {
   const map1 = fromJS(object);
   return getIn(map1, array, undefined);
-};
+}
